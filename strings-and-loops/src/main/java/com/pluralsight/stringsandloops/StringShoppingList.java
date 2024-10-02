3439e629ae346471ec1576559784e6b1c412d160
Nicholas Mulvaine <nmulvaine@icloud.com>
2024-10-02 13:49:56 -0400

    Added URL of Github repo

---
+++ strings-and-loops/src/main/java/com/pluralsight/stringsandloops/StringShoppingList.java
@@ -0,0 +1,15 @@
+package com.pluralsight.stringsandloops;
+
+public class StringShoppingList {
+    public static void main(String[] args) {
+        String shopList = "Eggs, Pasta, Milk, Cheese, Rice, Bread";
+        System.out.printf(shopList);
+    }
+
+    public static void smartList(){
+
+    }
+
+    }
+
+
