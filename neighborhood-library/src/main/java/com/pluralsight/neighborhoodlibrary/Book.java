09bb96c325edb0e3d1969b7a714ccb9926122933
Nicholas Mulvaine <nmulvaine@icloud.com>
2024-10-07 16:14:01 -0400

    Updated Neighborhood Library Project

---
+++ neighborhood-library/src/main/java/com/pluralsight/neighborhoodlibrary/Book.java
@@ -0,0 +1,27 @@
+package com.pluralsight.neighborhoodlibrary;
+
+
+public class Book {
+
+    public static void bookStorage(String[] args) {
+        String title;
+        String isbn;
+        String id;
+        Book t, t1, t2, t3, t4, t5;
+
+
+    }
+}
+  /*String id;
+    String isbn;
+    String title;
+    Boolean isCheckedOut;
+    String checkedOutTo;
+    String bookTitle;
+   */
+
+
+
+
+
+
