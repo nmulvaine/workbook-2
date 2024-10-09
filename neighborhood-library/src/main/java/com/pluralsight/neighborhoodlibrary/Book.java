226b35bcb6c1acc193a3e07b0988a332cc95a7ff
Nicholas Mulvaine <nmulvaine@icloud.com>
2024-10-09 08:06:42 -0400

    Added book obj

--- neighborhood-library/src/main/java/com/pluralsight/neighborhoodlibrary/Book.java
+++ neighborhood-library/src/main/java/com/pluralsight/neighborhoodlibrary/Book.java
@@ -1,57 +1,66 @@
 package com.pluralsight.neighborhoodlibrary;
+import java.lang.Object;
+import java.lang.String.*;
+
+public class Book {
+    private String title;
+    private String isbn;
+    private String id;
+
+    public Book (String title, String isbn, String id) {
+    this.title = title;
+    this.isbn = isbn;
+    this.id = id;
+    }
+
+    public String getTitle() {
+    return title;
+    }
+
+    public String getIsbn() {
+    return isbn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+        Object[] bookList = new Object[]{
+                "The Pragmatic Programmer", "978-0201616224", 1,
+                "Clean Code", "978-0132350884", 2,
+                "Design Patterns", "978-0201633610", 3,
+                "Code Complete", "978-0735619678", 4,
+                "Refactoring", "978-0201485677", 5,
+                "Head First Design Patterns", "978-0596007126", 6,
+                "Introduction to Algorithms", "978-0262033848", 7,
+                "The Mythical Man-Month", "978-0201835953", 8,
+                "Effective Java", "978-0134685991", 9,
+                "You Don't Know JS: Scope & Closures", "978-1491904152", 10,
+                "Java Concurrency in Practice", "978-0321349606", 11,
+                "The Art of Computer Programming", "978-0201896831", 12,
+                "Structure and Interpretation of Computer Programs", "978-0262510875", 13,
+                "Clean Architecture", "978-0134494166", 14,
+                "Working Effectively with Legacy Code", "978-0131177055", 15,
+                "Patterns of Enterprise Application Architecture", "978-0321127426", 16,
+                "Continuous Delivery", "978-0321601919", 17,
+                "Building Microservices", "978-1491950357", 18,
+                "Cracking the Coding Interview", "978-0984782857", 19,
+                "Artificial Intelligence: A Modern Approach", "978-0136042594", 20
+        };
+    private String toString (){
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", id=" + id +
+                '}';
+    }
 
 
-public class Book
-{
-
-    public static void bookStorage(String[] args)
-    {
-        class BookInfo
-        {
-            private String title;
-            private String isbn;
-            private String id;
-            private Book t, t1, t2, t3, t4, t5,
-                    t6, t7, t8, t9, t10,
-                    t11, t12, t13, t14, t15, t16,
-                    t17, t18, t19, t20;
-
-            for
-            i in; 1...20
-
-            {
-                this.Book = t;
-            }
-
-        }
 
 
-        BookInfo bookList[] = {
-                ("The Pragmatic Programmer", "978-0201616224", "1"),
-        ("Clean Code", "978-0132350884", "2"),
-        ("Design Patterns", "978-0201633610", "3"),
-        ("Code Complete", "978-0735619678", "4"),
-        ("Refactoring", "978-0201485677", "5"),
-        ("Head First Design Patterns", "978-0596007126", "6"),
-        ("Introduction to Algorithms", "978-0262033848", "7"),
-        ("The Mythical Man-Month", "978-0201835953", "8"),
-        ("Effective Java", "978-0134685991", "9"),
-        ("You Don't Know JS: Scope & Closures", "978-1491904152", "10"),
-        ("Java Concurrency in Practice", "978-0321349606", "11"),
-        ("The Art of Computer Programming", "978-0201896831", "12"),
-        ("Structure and Interpretation of Computer Programs", "978-0262510875", "13"),
-        ("Clean Architecture", "978-0134494166", "14"),
-        ("Working Effectively with Legacy Code", "978-0131177055", "15"),
-        ("Patterns of Enterprise Application Architecture", "978-0321127426", "16"),
-        ("Continuous Delivery", "978-0321601919", "17"),
-        ("Building Microservices", "978-1491950357", "18"),
-        ("Cracking the Coding Interview", "978-0984782857", "19")
-        ("Artificial Intelligence: A Modern Approach", "978-0136042594", "20");
-
-        }
 
     }
-}
+
 
 /*String id;
     String isbn;
